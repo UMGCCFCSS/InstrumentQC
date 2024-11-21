9e27463183c4ba7429717fd0744a9a094ee11782
David Rach <drach@som.umaryland.edu>
2024-11-21 16:53:11 -0500

    Update with CS data from today

--- Examples_FlagRemoval.R
+++
@@ -1,9 +0,0 @@
-Main <- file.path("C:", "Users", "12692", "Documents", "InstrumentQC")
-AnyFlags <- list.files(Main, pattern="Flag.csv", full.names=TRUE)
-
-if (!length(AnyFlags) == 0){
-  TheCSV <- file.path(Main, "Flag.csv")
-  file.remove(TheCSV)
-}
-
-
